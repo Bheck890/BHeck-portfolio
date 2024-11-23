1a3cd963c60bc0fc417616555bbbed7cc26a15e5
Bailey Heck <bheck756@gmail.com>
2024-11-23 12:47:08 -0500

    Updated Resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -14,5 +14,5 @@ export const personalData = {
   stackOverflow: 'https://stackexchange.com/users/25428119/bailey-heck',
   leetcode: "https://leetcode.com/u/uPkrS96Pkp/",
   devUsername: "bheck",
-  resume: "https://drive.google.com/file/d/1JElL-qcVsSh5SwA9GBWObGHeORPj2wRJ/view",
+  resume: "https://drive.google.com/file/d/1WWAUxjwnyUxYAvqetvNDZdc7pz-6oS4N/view",
 }
