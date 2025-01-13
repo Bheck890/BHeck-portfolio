4986fb9ec2fe4dc2c3fb9dd9b2ded9f0a9e4c0fd
Bailey Heck <47249563+Bheck890@users.noreply.github.com>
2025-01-12 20:13:36 -0500

    Updated Intro and added first big project

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Bailey Heck",
   profile: '/profile.png',
   designation: "Software Engineer",
-  description: "My name is Bailey Heck. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. My core skill is based on Type Script and I love to do most of the things using new technologies like flutter. I am available for any kind of job opportunity that suits my skills and interests. I am also good with Computer Hardware and the Information Technology Field with Servers, and management.",
+  description: "My name is Bailey Heck. I am a professional and enthusiastic programmer with a passion for problem-solving and exploring new technologies. I am a quick learner with a self-driven attitude, always eager to expand my knowledge. My core skills are based on TypeScript, and I enjoy working with modern technologies like Flutter. I have hands-on experience with SQL, having completed three projects involving big data, managing users in databases, and setting up notification systems within databases. Additionally, I have a strong background in Computer Hardware and IT, including server management and system administration. Having lived in New Jersey, Texas, and Florida, I bring a diverse perspective to my work. I am open to job opportunities that align with my skills and interests, ready to contribute my expertise to innovative projects.",
   email: 'bheck745@gmail.com',
   phone: '+15616326677',
   address: 'Fort Myers, Florida',
