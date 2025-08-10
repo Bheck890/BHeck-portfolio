f885173d4733d923b25237aa51819365ea42d1f7
Bailey Heck <47249563+Bheck890@users.noreply.github.com>
2025-08-10 15:03:50 -0600

    Rick Roll Update
    To protect private stuff on the front end

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,8 +3,8 @@ export const personalData = {
   profile: '/profile.png',
   designation: "Software Engineer",
   description: "My name is Bailey Heck. I am a professional and enthusiastic programmer with a passion for problem-solving and exploring new technologies. I am a quick learner with a self-driven attitude, always eager to expand my knowledge. My core skills are based on TypeScript, and I enjoy working with modern technologies like Flutter. I have hands-on experience with SQL, having completed three projects involving big data, managing users in databases, and setting up notification systems within databases. Additionally, I have a strong background in Computer Hardware and IT, including server management and system administration. I also self-taught Java in high school, further developing my programming foundation and adaptability. Having lived in New Jersey, Texas, and Florida, I bring a diverse perspective to my work. I am open to job opportunities that align with my skills and interests, ready to contribute my expertise to innovative projects.",
-  email: 'bheck745@gmail.com',
-  phone: '+15616326677',
+  email: 'Rick@gmail.com',
+  phone: '+1248-434-5508',
   address: 'Port Charlotte, Florida',
   github: 'https://github.com/bheck890',
   facebook: 'https://www.facebook.com/BHeck89/',
@@ -14,5 +14,5 @@ export const personalData = {
   stackOverflow: 'https://stackexchange.com/users/25428119/bailey-heck',
   leetcode: "https://leetcode.com/u/uPkrS96Pkp/",
   devUsername: "bheck",
-  resume: "https://drive.google.com/file/d/1qJAM9_nkeUYxvGDtbWhM4hvZ_30J1bvt/view",
+  resume: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
 }
